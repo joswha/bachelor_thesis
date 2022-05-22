dbc1cc4d5840ff9b01262f4cc98b5dc7dc4bf425
joswha <vladtoie@gmail.com>
2022-05-22 16:43:02 +0200

    initial official commit

---
+++ automatoor.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import subprocess
+from mobsftester import *
+
+"""
+Program that handles running the following tools, saving outputs in a subsequent folder:
+
+    1. apkid
+    2. apkleaks
+    3. mobsf
+    4. flowdroid
+    5. d2j-dex2jar + dependency-check on the newly created jar file
+
+'"""
+
+def run_apkid(_name):
+    """
+    Runs apkid on the apk file.
+    """
+    print(f"Running apkid on {_name}")
+
+    # Run apkid
+    apkid_cmd = f"apkid -v {_name} > apkid_output/{_name[:-4]}_apkid.txt"
+    subprocess.call(apkid_cmd, shell = True)
+
+def run_apkleaks(_name):
+    """
+    Runs apkleaks on the apk file.
+    """
+    print(f"Running apkleaks on {_name}")
+
+    # Run apkleaks
+    apkleaks_cmd = f"apkleaks -f {_name} -o apkleaks_output/{_name[:-4]}_apkleaks.txt"
+    subprocess.call(apkleaks_cmd, shell = True)
+
+def run_d_check(_name):
+    """
+    Runs dependency-check on the jar file.
+    """
+    print(f"Running dex2jar and dependency-check on {_name}")
+
+    # Run dex2jar
+    d2j_cmd = f"d2j-dex2jar {_name} -o temp_jar/{_name[:-4]}.jar"
+    subprocess.call(d2j_cmd, shell = True)
+
+    # Run dependency-check
+    d_check_cmd_json = f"dependency-check -s temp_jar/{_name[:-4]}.jar -f JSON -o dependencycheck_output/{_name[:-4]}"
+    subprocess.call(d_check_cmd_json, shell = True)
+    d_check_cmd_html = f"dependency-check -s temp_jar/{_name[:-4]}.jar -f HTML -o dependencycheck_output/{_name[:-4]}"
+    subprocess.call(d_check_cmd_html, shell = True)
+
+# def run_flowdroid(_name):
+    # """
+    # Runs flowdroid on the apk file.
+    # """
+    # print(f"Running flowdroid on {_name}")
+
+    # # Run flowdroid
+    # flowdroid_cmd = f"flowdroid -i {_name} -o flowdroid_output/{_name[:-4]}_flowdroid.txt"
+    # subprocess.call(flowdroid_cmd, shell = True)
+
+def run_mobsf(_name):
+    """
+    Runs mobsf on the apk file.
+    """
+    RESP = upload(_name)
+    scan(RESP)
+    json_resp(RESP)
+    pdf(RESP, _name[:-4])
+
+def create_output_folders():
+    if not os.path.exists("apkid_output"):
+        os.makedirs("apkid_output")
+    if not os.path.exists("apkleaks_output"):
+        os.makedirs("apkleaks_output")
+    if not os.path.exists("mobsf_output"):
+        os.makedirs("mobsf_output")
+    if not os.path.exists("flowdroid_output"):
+        os.makedirs("flowdroid_output")
+    if not os.path.exists("temp_jar"):
+        os.makedirs("temp_jar")
+    if not os.path.exists("dependencycheck_output"):
+        os.makedirs("dependencycheck_output")
+
+if __name__ == "__main__":
+
+    # List all the apk files form current working directory.
+    apk_files = [f for f in os.listdir(".") if f.endswith(".apk")]
+
+    # Create output folders, if they don't exist.
+    create_output_folders()
+
+    # Run the tools on all the apk files.
+    for apk in apk_files:
+        run_apkid(apk)
+        run_apkleaks(apk)
+        run_d_check(apk)
+        run_mobsf(apk)
---
+++ mobsftester.py
@@ -0,0 +1,63 @@
+"""
+MOBSF REST API Python Requests
+"""
+
+import json
+import requests
+import os
+import sys
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+SERVER = "http://127.0.0.1:8000"
+APIKEY = '4836a227d768f96dc7d1c7521b8baa571f064bf4ce11cdc4630dfbca5d7f0559'
+
+# Originally from https://gist.github.com/ajinabraham/0f5de3b0c7b7d3665e54740b9f536d81
+
+def upload(_name):
+    """Upload File"""
+    print("Uploading file")
+    multipart_data = MultipartEncoder(fields={'file': (_name, open(_name, 'rb'), 'application/octet-stream')})
+    headers = {'Content-Type': multipart_data.content_type, 'Authorization': APIKEY}
+    response = requests.post(SERVER + '/api/v1/upload', data=multipart_data, headers=headers)
+    print(response.text)
+    return response.text
+
+
+def scan(data):
+    """Scan the file"""
+    print("Scanning file")
+    post_dict = json.loads(data)
+    headers = {'Authorization': APIKEY}
+    response = requests.post(SERVER + '/api/v1/scan', data=post_dict, headers=headers)
+    print(response.text)
+
+
+def pdf(data, _name):
+    """Generate PDF Report"""
+    print("Generate PDF report")
+    headers = {'Authorization': APIKEY}
+    data = {"hash": json.loads(data)["hash"]}
+    response = requests.post(SERVER + '/api/v1/download_pdf', data=data, headers=headers, stream=True)
+    with open(f"mobsf_output/{_name}_report.pdf", 'wb') as flip:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                flip.write(chunk)
+    print(f"Report saved as mobsf_output/{_name}.pdf")
+
+
+def json_resp(data):
+    """Generate JSON Report"""
+    print("Generate JSON report")
+    headers = {'Authorization': APIKEY}
+    data = {"hash": json.loads(data)["hash"]}
+    response = requests.post(SERVER + '/api/v1/report_json', data=data, headers=headers)
+    print(response.text)
+
+
+def delete(data):
+    """Delete Scan Result"""
+    print("Deleting Scan")
+    headers = {'Authorization': APIKEY}
+    data = {"hash": json.loads(data)["hash"]}
+    response = requests.post(SERVER + '/api/v1/delete_scan', data=data, headers=headers)
+    print(response.text)
