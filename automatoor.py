68bd53abe306c848c8691806b1cd2a663c05808f
joswha <vladtoie@gmail.com>
2022-05-31 19:23:46 +0200

    added: 400 outputs of the larger dataset

--- automatoor.py
+++ automatoor.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 from mobsftester import *
+import time
 
 # pylint: disable=pointless-string-statement
 """
@@ -23,7 +24,17 @@ def run_apkid(_name):
 
     # Run apkid
     apkid_cmd = f"apkid -v apps/{_name} > apkid_output/{_name[:-4]}_apkid.txt"
-    subprocess.call(apkid_cmd, shell = True)
+
+    try:
+        subprocess.run(apkid_cmd, shell = True, timeout = 60, check = True)
+    except subprocess.TimeoutExpired:
+        
+        # Add a new line to the timeouts.txt file
+        with open("timeouts.txt", "a") as f:
+            f.write(f"apkid: {_name}\n")
+
+        print("TIMEOUT + apkid timed out + TIMEOUT on the following app: " + _name)
+        return
 
 def run_apkleaks(_name):
     """
@@ -33,7 +44,17 @@ def run_apkleaks(_name):
 
     # Run apkleaks
     apkleaks_cmd = f"apkleaks -f apps/{_name} -o apkleaks_output/{_name[:-4]}_apkleaks.txt"
-    subprocess.call(apkleaks_cmd, shell = True)
+
+    try:
+        subprocess.run(apkleaks_cmd, shell = True, timeout = 150, check = True)
+    except subprocess.TimeoutExpired:
+
+        # Add a new line to the timeouts.txt file
+        with open("timeouts.txt", "a") as f:
+            f.write(f"apkleaks: {_name}\n")
+
+        print("TIMEOUT + apkleaks timed out + TIMEOUT on the following app: " + _name)
+        return
 
 def run_d_check(_name):
     """
@@ -41,15 +62,28 @@ def run_d_check(_name):
     """
     print(f"Running dex2jar and dependency-check on {_name}")
 
-    # Run dex2jar
-    d2j_cmd = f"d2j-dex2jar apps/{_name} -o temp_jar/{_name[:-4]}.jar"
-    subprocess.call(d2j_cmd, shell = True)
 
-    # Run dependency-check
-    d_check_cmd_json = f"dependency-check -s temp_jar/{_name[:-4]}.jar -f JSON -o dependencycheck_output/{_name[:-4]}"
-    subprocess.call(d_check_cmd_json, shell = True)
-    d_check_cmd_html = f"dependency-check -s temp_jar/{_name[:-4]}.jar -f HTML -o dependencycheck_output/{_name[:-4]}"
-    subprocess.call(d_check_cmd_html, shell = True)
+    try:
+        # Run dex2jar
+        d2j_cmd = f"d2j-dex2jar apps/{_name} -o temp_jar/{_name[:-4]}.jar"
+        subprocess.run(d2j_cmd, shell = True, timeout = 20, check = True)
+
+        # Run dependency-check
+        d_check_cmd_json = f"dependency-check -n -s temp_jar/{_name[:-4]}.jar -f JSON -o dependencycheck_output/{_name[:-4]}"
+        # had to add -n so that no update is done, this got me a lot of errors
+        subprocess.run(d_check_cmd_json, shell = True, timeout = 40, check = True)
+        
+        # d_check_cmd_html = f"dependency-check -s temp_jar/{_name[:-4]}.jar -f HTML -o dependencycheck_output/{_name[:-4]}"
+        # subprocess.run(d_check_cmd_html, shell = True, timeout = 60, check = True)
+
+    except subprocess.TimeoutExpired:
+
+        # Add a new line to the timeouts.txt file
+        with open("timeouts.txt", "a") as f:
+            f.write(f"d_check: {_name}\n")
+
+        print("TIMEOUT + dependency-check timed out + TIMEOUT on the following app: " + _name)
+        return
 
 def run_flowdroid(_name):
     """
@@ -62,7 +96,18 @@ def run_flowdroid(_name):
     # Run flowdroid
     # flowdroid -a ../bachelor_thesis/apps/cam3.apk -p /Users/vlad/Library/Android/sdk/platforms -s soot-infoflow-android/SourcesAndSinks.txt
     flowdroid_cmd = f"java -jar {flow_droid_folder}/soot-infoflow-cmd/target/soot-infoflow-cmd-jar-with-dependencies.jar -s {flow_droid_folder}/soot-infoflow-android/SourcesAndSinks.txt -a apps/{_name} -p /Users/vlad/Library/Android/sdk/platforms -o flowdroid_output/{_name[:-4]}_flowdroid.xml"
-    subprocess.call(flowdroid_cmd, shell = True)
+
+    try:
+        subprocess.run(flowdroid_cmd, shell = True, timeout = 100, check = True)
+    except subprocess.TimeoutExpired:
+        
+        # Add a new line to the timeouts.txt file
+        with open("timeouts.txt", "a") as f:
+            f.write(f"flowdroid: {_name}\n")
+
+        print("TIMEOUT + flowdroid timed out + TIMEOUT on the following app: " + _name)
+        return
+    
 
 def run_mobsf(_name):
     """
@@ -93,17 +138,31 @@ def create_output_folders():
 if __name__ == "__main__":
 
     # List all the apk files form current working directory.
-    apk_files = [f for f in os.listdir("apps/") if f.endswith(".apk")]
+    # got to 38 after first run -> 37'th index
+    # second time(internet dc -> apkleaks doesn't work) -> 55
+    apk_files = [f for f in os.listdir("apps/") if f.startswith("installer") and f.endswith(".apk")]
+
+    apk_files = apk_files[55:]
 
     # Create output folders, if they don't exist.
     create_output_folders()
 
+    start_time = time.time()
+
     # Run the tools on all the apk files.
     for apk in apk_files:
-        if apk == "all1.apk" or apk == "web2.apk":
-            continue
-        # run_apkid(apk)
-        # run_apkleaks(apk)
-        # run_d_check(apk)
-        # run_mobsf(apk)
+        run_apkid(apk)
+
+        # Run apkleaks
+        run_apkleaks(apk)
+
+        # Run dex2jar and dependency-check
+        run_d_check(apk)
+
+        # Run mobsf
+        run_mobsf(apk)
+
+        # Run flowdroid
         run_flowdroid(apk)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
